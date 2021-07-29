024ab6c795860015aa0714233e279230a0921421
Taekhun <nth9708@naver.com>
2021-07-30 02:56:13 +0900

    Feat: random,unlike 함수(mock) 구현 Product 페이지에서, localStorage 함수 구현 전에 사용할 mock random, unlike 함수 구현

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,36 +1,76 @@
 import React, { Component, createRef } from 'react';
 import { fetchGet } from '../utils/fetches';
 import styled from 'styled-components';
+import { createBrowserHistory } from 'history';
 
 export default class Product extends Component {
   state = {
     product: {
       id: Math.floor(Math.random() * 100),
-      title: '나이키 데이브레이크',
-      brand: '나이키',
-      price: '1500000',
+      title: '',
+      brand: '',
+      price: '',
       unlike: false,
     },
   };
 
-  componentDidMount() {}
+  history = createBrowserHistory;
+  randomRef = createRef();
+  unlikeRef = createRef();
+
+  componentDidMount() {
+    this.getProduct(this.state.product.id);
+    this.randomRef.current.addEventListener('click', this.randomClicked);
+    this.unlikeRef.current.addEventListener('click', this.unlikeClicked);
+  }
+
+  //TODO: replace to localStorage func
+  getProduct = async (id) => {
+    const res = await fetchGet(`http://localhost:3000/data/productData.json`);
+    const products = await res.json();
+    this.setState({ product: products[id] });
+  };
+
+  //TODO: change to localStorage Func
+  putProduct = async ({ id, unlike }) => {
+    console.log(`[put product] id :${id} unlike : ${unlike}`);
+  };
+  //TODO: change to localStorage Func
+  putRecentList = async ({ id, unlike }) => {
+    console.log(`[push to recentList] id :${id} unlike : ${unlike}`);
+  };
+
+  randomClicked = () => {
+    this._id = Math.floor(Math.random() * 100);
+    this.putRecentList({ ...this.state.product, unlike: true, id: this._id });
+    this.getProduct(this._id);
+  };
+
+  unlikeClicked = () => {
+    this._id = Math.floor(Math.random() * 100);
+    this.putProduct({ ...this.state.product, unlike: false, id: this._id });
+    this.getProduct(this._id);
+  };
 
   render() {
     const { title, brand, price } = this.state.product;
 
     return (
-      <div>
-        <h1>Product Page</h1>
-        <h3>{title}</h3>
-        <h3>{brand}</h3>
-        <h3>{price}</h3>
-        <RandomButton>리셋</RandomButton>
-        <UnlikeButton>관심없음</UnlikeButton>
-      </div>
+      <>
+        <div className='product--card'>
+          <h1>Product Page</h1>
+          <h3>{title}</h3>
+          <h3>{brand}</h3>
+          <h3>{price}</h3>
+        </div>
+        <RandomButton ref={this.randomRef}>리셋</RandomButton>
+        <UnlikeButton ref={this.unlikeRef}>관심없음</UnlikeButton>
+      </>
     );
   }
 }
 
+//TODO: CSS Styling
 const UnlikeButton = styled.button`
   border: 1px solid black;
   border-radius: 25px;
