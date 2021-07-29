adf53b22b9377e01187c37ed83fb18ba3298bdd9
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-30 03:23:13 +0900

    feat: RecentList component 구성
    - mock data fetch

--- src/pages/RecentList.jsx
+++ src/pages/RecentList.jsx
@@ -1,7 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
+import styled from 'styled-components';
+import { fetchGet } from '../utils/fetches';
 
 export default class RecentList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentList: [],
+    };
+  }
+  componentWillMount() {
+    fetchGet('data/productData.json')
+      .then((res) => res.json())
+      .then((data) => this.setState({ recentList: data.slice(0, 10) }));
+  }
   render() {
-    return <div></div>;
+    const { recentList } = this.state;
+    console.log(recentList);
+    return (
+      <RecentListDiv>
+        {recentList.map((card) => {
+          const { brand, price, title } = card;
+          return (
+            <ProductCardDiv>
+              {brand} {price} {title}
+            </ProductCardDiv>
+          );
+        })}
+      </RecentListDiv>
+    );
   }
 }
+
+const RecentListDiv = styled.div``;
+const ProductCardDiv = styled.div`
+  width: 200px;
+  height: 100px;
+  background-color: skyblue;
+`;
