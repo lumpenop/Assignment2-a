117d3b134dc21beb95ae2e0e292d609d5d9afda2
sanghunlee <simoniful0gmail.com>
2021-07-30 01:15:48 +0900

    Feat: modify comp setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './Routes';
 import GlobalStyle from './styles/GlobalStyle';
 
 ReactDOM.render(
   <>
     <GlobalStyle />
-    <App />
+    <Routes />
   </>,
   document.getElementById('root'),
 );
