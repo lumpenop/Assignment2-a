2d46726ca569f45a2282f534b4dd9b8739ace56a
Taekhun <nth9708@naver.com>
2021-07-30 23:47:55 +0900

    Feat: 고정 MAX config파일에 추가

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const ERROR_MESSAGE = 'Failed to fetch data, please check your API or internet connection';
-
-export const RANDOM_0_99 = () => Math.floor(Math.random() * 99);
+export const MAX = 100;
+export const RANDOM_0_MAX = () => Math.floor(Math.random() * (MAX - 1));
