bae316fd5357c71ad1e9af81de5f67e2e3aab83f
Taekhun <nth9708@naver.com>
2021-07-30 21:33:32 +0900

    Feat: 랜덤한 0~99 수 뽑는 util 추가

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
-export const ERROR_MESSAGE =
-  'Failed to fetch data, please check your API or internet connection';
+export const ERROR_MESSAGE = 'Failed to fetch data, please check your API or internet connection';
+
+export const RANDOM_0_99 = () => Math.floor(Math.random() * 99);
