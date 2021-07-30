07971c3da0a3e962c6e8cc75108bbc0108419dee
Taekhun <nth9708@naver.com>
2021-07-30 23:49:22 +0900

    Feat: 최근 이력 추가, 현재 상품, 관심없는 상품 제외하여 띄워주는 기능 구현

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,12 +2,15 @@ import React, { Component, createRef } from 'react';
 import { fetchGet } from '../utils/fetches';
 import styled from 'styled-components';
 import { createBrowserHistory } from 'history';
-import { saveStore } from '../utils/storage';
+import { saveStore, getStore, updateProduct } from '../utils/storage';
+import { MAX, RANDOM_0_MAX } from '../utils/config';
+
+// 컴디업후 최근 이력 추가하려했으나, 로직상, onClick 이벤트 맨 마지막에 갱신하는게 자연스러워서 거기로 뺐습니다.
 
 export default class Product extends Component {
   state = {
     product: {
-      id: Math.floor(Math.random() * 100),
+      id: 0,
       title: '',
       brand: '',
       price: '',
@@ -20,73 +23,89 @@ export default class Product extends Component {
   unlikeRef = createRef();
 
   componentDidMount() {
-    this.getProduct(this.state.product.id);
-    this.randomRef.current.addEventListener('click', this.randomClicked);
-    this.unlikeRef.current.addEventListener('click', this.unlikeClicked);
+    this.fetchProducts();
+    this.randomRef.current.addEventListener('click', this.onClickRandom);
+    this.unlikeRef.current.addEventListener('click', this.onClickUnlike);
   }
 
-  componentDidUpdate(_, prevState) {
-    if (prevState.product !== this.state.product) {
-      saveStore('recentViewed', this.state.product);
+  // 첫 렌더링시, fetch data
+  // ls에 이미 있을시 fetch X, 랜덤만 해줌
+  fetchProducts = async () => {
+    if (getStore('productList').length) {
+      const randomProduct = this.getRandomProduct(RANDOM_0_MAX());
+      const assignedProduct = Object.assign(randomProduct, { unlike: false });
+      this.setState({ product: { ...this.state.product, ...assignedProduct } });
+      return;
     }
-  }
 
-  getProduct = async (id) => {
     const res = await fetchGet(`http://localhost:3000/data/productData.json`);
     const products = await res.json();
     localStorage.setItem(
       'productList',
       JSON.stringify(products.map((el, idx) => Object.assign({ id: idx, unlike: false }, el)))
     );
-    const ramdomProducts = JSON.parse(localStorage.getItem('productList'));
-    this.setState({ product: ramdomProducts[id] });
+
+    const randomProduct = this.getRandomProduct(RANDOM_0_MAX());
+    const assignedProduct = Object.assign(randomProduct, { unlike: false });
+    this.setState({ product: { ...this.state.product, ...assignedProduct } });
   };
 
-  putProduct = async ({ id, unlike }) => {
-    console.log(`[put product] id :${id} unlike : ${unlike}`);
+  // TODO : 똑같은 상품 나올 시 앞으로 빼주는거
+  getRandomProduct = (_id) => {
+    let randomProduct = getStore('productList')[_id];
+    while (true) {
+      // TODO 구현되면, 이거 필요 없음
+      if (getStore('recentViewed').length === MAX) {
+        localStorage.clear();
+      }
+
+      // 현재와 다른 제품, unlike === false 아니면 다시 돌리기
+      if (this.state.product.id !== _id && getStore('productList')[_id].unlike === false) {
+        break;
+      }
+
+      _id = RANDOM_0_MAX();
+    }
+
+    return Object.assign(randomProduct, { id: _id }); // 가져온 product에 id 갱신해서 리턴(바뀔수도있으므로)
   };
 
-  putRecentList = async () => {
-    // localStorage.setItem('recentViewed', JSON.stringify(this.state.product));
-    // const prevViewedLog = JSON.parse(localStorage.getItem('recentViewed'));
-    // console.log(prevViewedLog);
-    // localStorage.setItem(
-    //   'recentViewed',
-    //   JSON.stringify(prevViewedLog.concat(this.state.product)),
-    // );
+  onClickRandom = () => {
+    const randomProduct = this.getRandomProduct(RANDOM_0_MAX());
+    const assignedProduct = Object.assign(randomProduct, { unlike: false }); // 가져온 product에 unlike값할당
+    this.setState({ product: { ...this.state.product, ...assignedProduct } }); // newProduct setState
+    updateProduct(assignedProduct); // ls에 product 정보 갱신
+    saveStore('recentViewed', assignedProduct); // ls에 최근이력 추가
   };
 
-  randomClicked = () => {
-    this._id = Math.floor(Math.random() * 100);
-    this.putRecentList(this.state.product);
-    this.getProduct(this._id);
-  };
-
-  unlikeClicked = () => {
-    this._id = Math.floor(Math.random() * 100);
-    this.putProduct({ ...this.state.product, unlike: true, id: this._id });
-    this.getProduct(this._id);
+  onClickUnlike = () => {
+    const randomProduct = this.getRandomProduct(RANDOM_0_MAX());
+    const assignedProduct = Object.assign(randomProduct, { unlike: true });
+    this.setState({ product: { ...this.state.product, ...assignedProduct } });
+    updateProduct(assignedProduct);
+    saveStore('recentViewed', assignedProduct);
   };
 
   render() {
     const { title, brand, price } = this.state.product;
 
     return (
-      <>
-        <div className='product--card'>
-          <h1>Product Page</h1>
+      <Container>
+        <RandomButton ref={this.randomRef}>랜덤</RandomButton>
+        <UnlikeButton ref={this.unlikeRef}>관심없음</UnlikeButton>
+        <div className='ProductCard'>
           <h3>{title}</h3>
           <h3>{brand}</h3>
           <h3>{price}</h3>
         </div>
-        <RandomButton ref={this.randomRef}>리셋</RandomButton>
-        <UnlikeButton ref={this.unlikeRef}>관심없음</UnlikeButton>
-      </>
+      </Container>
     );
   }
 }
 
 //TODO: CSS Styling
+const Container = styled.div``;
+
 const UnlikeButton = styled.button`
   border: 1px solid black;
   border-radius: 25px;
